0930da9f2e0f69d84df8c8dc28eb577d72fe7689
VillanuevaAndrea <villanueva94andrea@gmail.com>
2026-01-18 18:52:20 -0300

    logica del registro

--- registro.php
+++ registro.php
@@ -1,4 +1,33 @@
-<?php require 'conexion.php'; ?>
+<?php require 'conexion.php'; 
+
+
+$mensaje = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['usuario'];
+    $pass= $_POST['password'];
+
+    $pass_encriptada = password_hash($pass, PASSWORD_BCRYPT);
+
+    try{
+        $stmt = $conexion->prepare("insert into usuarios (usuario, password) VALUES (:u, :p)");
+        $stmt->bindParam(':u', $user);
+        $stmt->bindParam(':p', $pass_encriptada);
+
+        if ($stmt->execute()) {
+            $mensaje = "<p class='success'> Registro exitoso. <a href='login.php'>iniciar sesión</a></p>.";
+        }
+    } catch (PDOException $e) {
+        if ($e->getCode() == 23000) {
+            $mensaje = "<p class='error'>El nombre de usuario '$user'ya existe. Por favor, elige otro.</p>";
+        } else {
+            $mensaje = "<p class='error'> No se pudo registrar el usuario. Inténtelo de nuevo.</p>";
+        }
+    }
+}
+
+?>
+
 <?php include 'includes/header.php'; ?>
 
 <div class="container">
